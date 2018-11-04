9581f1b536518095096f14be15f0de247f9195b4
David Barrero <35556491+davidsar1@users.noreply.github.com>
2018-11-04 17:33:19 -0500

    Added Back4App Parse Server. Verified by installing on device and confirming "installation" class on Back4App dashboard

--- app/src/main/java/com/barrero/david/instagramclone/Signup.java
+++ app/src/main/java/com/barrero/david/instagramclone/Signup.java
@@ -3,11 +3,17 @@ package com.barrero.david.instagramclone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.parse.ParseInstallation;
+
 public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        // Save the current Installation to Back4App
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
     }
 }
