8495cf5f5054ba39497b2b17ed12ccb19bb4c151
David Barrero <35556491+davidsar1@users.noreply.github.com>
2018-11-04 22:12:43 -0500

    Signup, Login, and Logout functions added.

--- app/src/main/java/com/barrero/david/instagramclone/Login.java
+++ app/src/main/java/com/barrero/david/instagramclone/Login.java
@@ -1,14 +1,21 @@
 package com.barrero.david.instagramclone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class Login extends AppCompatActivity {
 
-    private EditText edtLoginNameOrEmail, edtLoginPassword;
+    private EditText edtLoginUsername, edtLoginPassword;
     private Button btnLogin;
     private TextView txtNoAccount;
 
@@ -17,12 +24,39 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        edtLoginNameOrEmail = findViewById(R.id.edtLoginNameOrEmail);
+        edtLoginUsername = findViewById(R.id.edtLoginUsername);
         edtLoginPassword = findViewById(R.id.edtLoginPassword);
 
         btnLogin = findViewById(R.id.btnLogin);
 
         txtNoAccount = findViewById(R.id.txtNoAccount);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logInInBackground(edtLoginUsername.getText().toString(), edtLoginPassword.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null && e == null) {
+                            Toast.makeText(Login.this, "Welcome " + user.get("username"), Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(Login.this, Welcome.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(Login.this, "Incorrect Username or Password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        txtNoAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Signup.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/barrero/david/instagramclone/Signup.java
+++ app/src/main/java/com/barrero/david/instagramclone/Signup.java
@@ -1,12 +1,18 @@
 package com.barrero.david.instagramclone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class Signup extends AppCompatActivity {
 
@@ -17,6 +23,7 @@ public class Signup extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle(R.string.signup_title);
         setContentView(R.layout.activity_signup);
 
         // Save the current Installation to Back4App
@@ -30,6 +37,41 @@ public class Signup extends AppCompatActivity {
         btnSignup = findViewById(R.id.btnSignup);
 
         txtHaveAccount = findViewById(R.id.txtHaveAccount);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ParseUser appUser = new ParseUser();
+                appUser.setEmail(edtSignupEmail.getText().toString());
+                appUser.setUsername(edtSignupUsername.getText().toString());
+                appUser.setPassword(edtSignupPassword.getText().toString());
+
+                appUser.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null && edtSignupPassword.getText().toString().equals(edtConfirmPassword.getText().toString())) {
+                            Toast.makeText(Signup.this, "welcome " + appUser.get("username"), Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(Signup.this, Welcome.class);
+                            startActivity(intent);
+
+                        } else if (e != null && edtSignupPassword == edtConfirmPassword) {
+                            Toast.makeText(Signup.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                        } else if (e == null && edtSignupPassword != edtConfirmPassword) {
+                            Toast.makeText(Signup.this, "The Passwords did not match", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        txtHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Signup.this, Login.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
