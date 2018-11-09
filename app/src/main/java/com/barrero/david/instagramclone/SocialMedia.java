b167f8749c05ff90a7be45fd80d7ba432a11ae97
David Barrero <35556491+davidsar1@users.noreply.github.com>
2018-11-09 00:43:28 -0500

    Tab Layout Setup

--- app/src/main/java/com/barrero/david/instagramclone/SocialMedia.java
+++ app/src/main/java/com/barrero/david/instagramclone/SocialMedia.java
@@ -1,25 +1,36 @@
 package com.barrero.david.instagramclone;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 import com.parse.ParseUser;
 
+
 public class SocialMedia extends AppCompatActivity {
+
+    private android.support.v7.widget.Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabAdapter tabAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
 
-        findViewById(R.id.btnLogout).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ParseUser.logOut();
-                finish();
-            }
-        });
+        setTitle(ParseUser.getCurrentUser().getUsername());
+
+        toolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager, false);
 
     }
 }
