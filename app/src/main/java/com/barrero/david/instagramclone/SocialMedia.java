86206dc10812e1db6a1bf4ddc21a1a37c159c5a9
David Barrero <35556491+davidsar1@users.noreply.github.com>
2018-11-12 23:38:09 -0500

    Implemented Menu with Image Upload and Logout

--- app/src/main/java/com/barrero/david/instagramclone/SocialMedia.java
+++ app/src/main/java/com/barrero/david/instagramclone/SocialMedia.java
@@ -1,11 +1,30 @@
 package com.barrero.david.instagramclone;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.ByteArrayOutputStream;
 
 
 public class SocialMedia extends AppCompatActivity {
@@ -14,6 +33,7 @@ public class SocialMedia extends AppCompatActivity {
     private ViewPager viewPager;
     private TabLayout tabLayout;
     private TabAdapter tabAdapter;
+    private ProgressBar progressBarSocialMedia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +51,91 @@ public class SocialMedia extends AppCompatActivity {
 
         tabLayout = findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager, false);
+
+        progressBarSocialMedia = findViewById(R.id.progressBarSocialMedia);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.postImageItem) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_DENIED) {
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 3000);
+            } else {
+                captureImage();
+            }
+        }
+
+        else if (item.getItemId() == R.id.logoutUserItem) {
+            ParseUser.getCurrentUser().logOut();
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 3000) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                captureImage();
+            }
+        }
+
+    }
+
+    private void captureImage() {
+
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 4000);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 4000 && resultCode == RESULT_OK && data != null) {
+            try {
+
+                Uri capturedImage = data.getData();
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), capturedImage);
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                byte[] bytes = byteArrayOutputStream.toByteArray();
+
+                ParseFile parseFile = new ParseFile("img.png", bytes);
+                ParseObject parseObject = new ParseObject("Photo");
+                parseObject.put("picture", parseFile);
+                parseObject.put("username", ParseUser.getCurrentUser().getUsername());
+                progressBarSocialMedia.setVisibility(View.VISIBLE);
+                parseObject.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Toast.makeText(SocialMedia.this, "Picture Uploaded", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(SocialMedia.this, "Unknown Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                        progressBarSocialMedia.setVisibility(View.GONE);
+                    }
+                });
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }
